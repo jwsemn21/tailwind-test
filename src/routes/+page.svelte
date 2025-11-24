0822ab806697d60a45c6d6ecf9b64852111babd0
jwsemn21 <joelwiseman1406@gmail.com>
2025-11-24 16:50:54 +0000

    just more stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="text-primary">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>Another message</p>
-<p>Yet Another message</p>
+<script>
+
+</script>
+
+
+
+<div>
+    <div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="text-center lg:text-left">
+      <h1 class="text-5xl font-bold">Joel's portfolio website coming soon!</h1>
+      <p class="py-6">
+        Needs work
+      </p>
+    </div>
+    <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div class="card-body">
+        <p class="mb-2">Get emailed when it is complete!</p>
+        <fieldset class="fieldset">
+          <input type="email" class="input" placeholder="Email" />
+          <button class="btn btn-neutral mt-4">Sign Up</button>
+        </fieldset>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
